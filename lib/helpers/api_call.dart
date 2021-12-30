1d9fbc88b7c03a9ae5e2b360e93b33ca03383487
Arjun <arjundoel@gmail.com>
2021-12-30 15:55:09 +0000

    get request to api success

--- lib/helpers/api_call.dart
+++ lib/helpers/api_call.dart
@@ -2,7 +2,13 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 Future getMeals() async {
-  try {} catch (e) {
+  try {
+    var url = 'http://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata';
+    var response = await http.get(Uri.parse(url));
+    var json = jsonDecode(response.body);
+    var meal = json['meals'][0];
+    print(meal['strMeal']);
+  } catch (e) {
     print(e.toString());
   }
 }
--- lib/pages/meal_view.dart
+++ lib/pages/meal_view.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:kitchen_space/helpers/api_call.dart';
 
-class MealView extends StatelessWidget {
+class MealView extends StatefulWidget {
   const MealView({Key? key}) : super(key: key);
+
+  @override
+  _MealViewState createState() => _MealViewState();
+}
+
+class _MealViewState extends State<MealView> {
+  var meals;
+
+  @override
+  void initState() {
+    getValues();
+  }
+
+  getValues() async {
+    await getMeals();
+    // setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
